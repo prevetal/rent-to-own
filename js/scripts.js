25ae64f73109ee0e8cad7afb0d2c7b9034d1b261
Vitaliy Sulimenko <prevetal@gmail.com>
2025-12-04 14:18:01 +0400

    Update

--- js/scripts.js
+++ js/scripts.js
@@ -88,6 +88,95 @@ document.addEventListener('DOMContentLoaded', function() {
 	})
 
 
+	// Product info
+	if ($('.product_info .images').length) {
+		const productThumbs = new Swiper('.product_info .thumbs .swiper', {
+			loop: false,
+			speed: 500,
+			watchSlidesProgress: true,
+			slideActiveClass: 'active',
+			slideVisibleClass: 'visible',
+			lazy: true,
+			spaceBetween: 1,
+			slidesPerView: 'auto',
+			navigation: {
+				nextEl: '.swiper-button-next',
+				prevEl: '.swiper-button-prev'
+			},
+		})
+
+		new Swiper('.product_info .big .swiper', {
+			loop: false,
+			speed: 500,
+			watchSlidesProgress: true,
+			slideActiveClass: 'active',
+			slideVisibleClass: 'visible',
+			spaceBetween: 0,
+			slidesPerView: 1,
+			lazy: true,
+			navigation: {
+				nextEl: '.swiper-button-next',
+				prevEl: '.swiper-button-prev'
+			},
+			thumbs: {
+				swiper: productThumbs
+			}
+		})
+	}
+
+
+	// Text image sliders
+	const textImageSliders = [],
+		textThumbsSliders = [],
+		textImages = document.querySelectorAll('.text_block .images_slider .big .swiper'),
+		textThumbs = document.querySelectorAll('.text_block .images_slider .thumbs .swiper')
+
+	textThumbs.forEach((el, i) => {
+		el.classList.add('text_thumbs_s' + i)
+
+		let options = {
+			loop: false,
+			speed: 500,
+			watchSlidesProgress: true,
+			slideActiveClass: 'active',
+			slideVisibleClass: 'visible',
+			lazy: true,
+			spaceBetween: 1,
+			slidesPerView: 'auto',
+			navigation: {
+				nextEl: '.swiper-button-next',
+				prevEl: '.swiper-button-prev'
+			},
+		}
+
+		textThumbsSliders.push(new Swiper('.text_thumbs_s' + i, options))
+	})
+
+	textImages.forEach((el, i) => {
+		el.classList.add('text_images_s' + i)
+
+		let options = {
+			loop: false,
+			speed: 500,
+			watchSlidesProgress: true,
+			slideActiveClass: 'active',
+			slideVisibleClass: 'visible',
+			spaceBetween: 0,
+			slidesPerView: 1,
+			lazy: true,
+			navigation: {
+				nextEl: '.swiper-button-next',
+				prevEl: '.swiper-button-prev'
+			},
+			thumbs: {
+				swiper: textThumbsSliders[i]
+			}
+		}
+
+		textImageSliders.push(new Swiper('.text_images_s' + i, options))
+	})
+
+
 	// Fancybox
 	Fancybox.defaults.autoFocus = false
 	Fancybox.defaults.trapFocus = false
@@ -132,6 +221,87 @@ document.addEventListener('DOMContentLoaded', function() {
 		Thumbs: {
 			autoStart: false
 		}
+	})
+
+
+	// Mini pop-up
+	$('.mini_modal_btn').click(function(e) {
+		e.preventDefault()
+
+		const modalId = $(this).data('modal-id')
+
+		if ($(this).hasClass('active')) {
+			$(this).removeClass('active')
+			$('.mini_modal').removeClass('active')
+
+			if (is_touch_device()) $('body').css('cursor', 'default')
+		} else {
+			$('.mini_modal_btn').removeClass('active')
+			$(this).addClass('active')
+
+			$('.mini_modal').removeClass('active')
+			$(modalId).addClass('active')
+
+			if (is_touch_device()) $('body').css('cursor', 'pointer')
+		}
+	})
+
+	// Close the popup when you click outside of it
+	$(document).click(e => {
+		if ($(e.target).closest('.modal_cont').length === 0) {
+			$('.mini_modal, .mini_modal_btn').removeClass('active')
+
+			if (is_touch_device()) $('body').css('cursor', 'default')
+		}
+	})
+
+
+	// Filter
+	$('.filter .search .clear_btn').click(function(e) {
+		e.preventDefault()
+
+		const modal  = $(this).closest('.mini_modal'),
+			input  = modal.find('.input'),
+			fields = modal.find('.field'),
+			count = $(this).closest('.modal_cont').find('.mini_modal_btn .count')
+
+		input.val('')
+		count.text('')
+		fields.show()
+		modal.find('input[type=checkbox]').prop('checked', false)
+	})
+
+	$(document).on('change', '.mini_modal input[type=checkbox]', function() {
+		const checkedCount = $(this)
+			.closest('.mini_modal')
+			.find('input[type=checkbox]:checked')
+			.length
+
+		const count = $(this).closest('.modal_cont').find('.mini_modal_btn .count')
+
+		checkedCount
+			? count.text(checkedCount)
+			: count.text('')
+	})
+
+	$('.filter .search .input').on('input', function() {
+		const val = $(this).val().toLowerCase(),
+			modal = $(this).closest('.mini_modal'),
+			fields = modal.find('.field'),
+			count = $(this).closest('.modal_cont').find('.mini_modal_btn .count')
+
+		fields.each(function() {
+			$(this).removeClass('hide')
+
+			const text = $(this).find('.checkbox .check + div').text().toLowerCase()
+
+			text.includes(val)
+				? $(this).removeClass('hide')
+				: $(this).addClass('hide')
+		})
+
+		count.text('')
+		modal.find('input[type=checkbox]').prop('checked', false)
 	})
 
 
